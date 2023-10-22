116bf6be53f15c26c4af12ac6adeef2f55d2fa07
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2023-10-22 01:58:25 -0400

    wastrer

--- src/main/java/me/aurelium/particles/Water.java
+++ src/main/java/me/aurelium/particles/Water.java
@@ -1,10 +1,11 @@
 package me.aurelium.particles;
 
+import me.aurelium.SuperParticle.GravityAffected;
 import me.aurelium.SuperParticle.Particle;
 
 import java.awt.*;
 
-public class Water extends Particle {
+public class Water extends GravityAffected {
     public Water() {
         this.temperature = 293.15F;
         this.weight=350;
@@ -21,9 +22,74 @@ public class Water extends Particle {
         return true;
     }
 
+    private boolean isAirOrBuoyant(Particle p) {
+        if(p instanceof Air)
+            return true;
+
+        if(p instanceof GravityAffected) {
+            return p.weight < this.weight;
+        }
+
+        return false;
+    }
+
     @Override
     public void interactionCheck(Particle[][] p) {
+        super.interactionCheck(p);
         //nothing?
 
+        if(p[1][1] instanceof Air) {
+            return;
+        }
+
+        boolean odd = System.currentTimeMillis() % 2 == 1;
+
+        if(isAirOrBuoyant(p[2][0])) {
+            Particle old = p[2][0];
+            p[2][0] = this;
+            p[1][1] = old;
+        } else if(isAirOrBuoyant(p[2][2])) {
+            Particle old = p[2][2];
+            p[2][2] = this;
+            p[1][1] = old;
+        } else {
+            if(odd) {
+                if(isAirOrBuoyant(p[1][0])) {
+                    Particle old = p[1][0];
+                    p[1][0] = this;
+                    p[1][1] = old;
+                }
+            } else {
+                if(isAirOrBuoyant(p[1][2])) {
+                    Particle old = p[1][2];
+                    p[1][2] = this;
+                    p[1][1] = old;
+                }
+            }
+        }
+
+        if(!(p[1][1] == this)) {
+            return;
+        }
+
+        if(p[2][1] instanceof GravityAffected) {
+            if(p[2][1].weight < this.weight) {
+                Particle old = p[2][1];
+                p[2][1] = this;
+                p[1][1] = old;
+            }
+        }
+
+        if(!(p[1][1] == this)) {
+            return;
+        }
+
+        if(p[0][1] instanceof GravityAffected) {
+            if(p[0][1].weight > this.weight) {
+                Particle old = p[0][1];
+                p[0][1] = this;
+                p[1][1] = old;
+            }
+        }
     }
 }
