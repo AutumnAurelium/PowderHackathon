7eca05fe0badbcf5ad486112c9135195a8d7afc6
allis <avf5633@psu.edu>
2023-10-22 02:17:58 -0400

    STEAM

--- src/main/java/me/aurelium/particles/Acid.java
+++ src/main/java/me/aurelium/particles/Acid.java
@@ -3,6 +3,7 @@ package me.aurelium.particles;
 
 import me.aurelium.particles.SuperParticle.Particle;
 import me.aurelium.particles.SuperParticle.SuperLiquid;
+import me.aurelium.particles.SuperParticle.SuperSolid;
 
 import java.awt.*;
 public class Acid extends SuperLiquid {
@@ -24,8 +25,19 @@ public class Acid extends SuperLiquid {
 
     @Override
     public void interactionCheck(Particle[][] p) {
-        //dissolves adjacent particle, destroyed in the process.
+        //dissolves adjacent particle, destroying acid in the process (turns both into air).
+        if (p[1][0] instanceof SuperSolid || p[1][0] instanceof SuperLiquid || p[1][0] instanceof Metal || p[1][0] instanceof Grass){
+            p[1][1]=new Air();
+            p[1][0]=new Air();
+        } else if (p[0][1] instanceof SuperSolid || p[0][1] instanceof SuperLiquid || p[0][1] instanceof Metal || p[0][1] instanceof Grass){
+            p[1][1]=new Air();
+            p[0][1]=new Air();
+        } else if (p[1][2] instanceof SuperSolid || p[1][2] instanceof SuperLiquid || p[1][2] instanceof Metal || p[1][2] instanceof Grass){
+            p[1][1]=new Air();
+            p[1][2]=new Air();
+        } else if (p[2][1] instanceof SuperSolid || p[2][1] instanceof SuperLiquid || p[2][1] instanceof Metal || p[2][1] instanceof Grass){
+            p[1][1]=new Air();
+            p[2][1]=new Air();
+        }
     }
-
-
 }
