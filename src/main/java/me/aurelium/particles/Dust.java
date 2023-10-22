d6a2cc4ad92f889977e237208bc6c3b0f7f618bc
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2023-10-22 10:00:34 -0400

    dust generic

--- src/main/java/me/aurelium/particles/Dust.java
+++ src/main/java/me/aurelium/particles/Dust.java
@@ -31,20 +31,6 @@ public class Dust extends SuperSolid {
         if(p[1][1] instanceof Air)
             return;
 
-        if(p[2][1] != null && yVelocity == 0) {
-            if ((p[2][1].canCollide(this))) {
-                if (p[2][0] instanceof Air) {
-                    Particle old = p[2][0];
-                    p[2][0] = this;
-                    p[1][1] = old;
-                } else if (p[2][2] instanceof Air) {
-                    Particle old = p[2][2];
-                    p[2][2] = this;
-                    p[1][1] = old;
-                }
-            }
-        }
-
         if (p[1][0] instanceof Water){
             p[1][1]=new Mud();
             p[1][0]=new Air();
--- src/main/java/me/aurelium/particles/SuperParticle/SuperSolid.java
+++ src/main/java/me/aurelium/particles/SuperParticle/SuperSolid.java
@@ -1,5 +1,27 @@
 package me.aurelium.particles.SuperParticle;
 
-public abstract class SuperSolid extends GravityAffected {
+import me.aurelium.particles.Air;
 
+public abstract class SuperSolid extends GravityAffected {
+    @Override
+    public void interactionCheck(Particle[][] p) {
+        super.interactionCheck(p);
+
+        if(p[1][1] != this)
+            return;
+
+        if(p[2][1] != null && yVelocity == 0) {
+            if ((p[2][1].canCollide(this))) {
+                if (p[2][0] instanceof Air) {
+                    Particle old = p[2][0];
+                    p[2][0] = this;
+                    p[1][1] = old;
+                } else if (p[2][2] instanceof Air) {
+                    Particle old = p[2][2];
+                    p[2][2] = this;
+                    p[1][1] = old;
+                }
+            }
+        }
+    }
 }
