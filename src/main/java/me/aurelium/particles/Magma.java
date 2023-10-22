20c334c9d468c53f2fe85449f9371feaafef315b
allis <avf5633@psu.edu>
2023-10-21 20:30:37 -0400

    magma

--- src/main/java/me/aurelium/particles/Magma.java
+++ src/main/java/me/aurelium/particles/Magma.java
@@ -8,13 +8,13 @@ public class Magma extends Particle {
 
     @Override
     public Color getColor() {
-        if (temperature>1400)
-            return new Color(255, 103, 102);
-        else if(temperature>600){
+        if (temperature<300)
+            return new Color(0,0,0);
+        else if(temperature<1400){
             int r= (int)(temperature*.175);
-            return new Color((int)(temperature*.175), 103, 102);
+            return new Color(255-(int)(temperature*.175), 0,0);
         }
         else
-            return new Color(105, 103, 102);
+            return new Color(255,0,0);
     }
 }
